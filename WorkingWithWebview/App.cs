f95574cfc492af4fe40eb8048ae58eccfbf03123
Robyn Thomas <robyn.ceri.thomas@gmail.com>
2020-07-29 15:55:34 +0100

    Use WebrtcWebPage instead of WebPage

--- WorkingWithWebview/App.cs
+++ WorkingWithWebview/App.cs
@@ -10,7 +10,7 @@ namespace WorkingWithWebview
 
 			tabs.Children.Add (new LocalHtml {Title = "Local" });
 			tabs.Children.Add (new LocalHtmlBaseUrl {Title = "BaseUrl" });
-			tabs.Children.Add (new WebPage { Title = "Web Page"});
+			tabs.Children.Add (new WebrtcWebPage { Title = "Web Page"});
 			tabs.Children.Add (new WebAppPage {Title ="External"});
 
 			MainPage = tabs;
